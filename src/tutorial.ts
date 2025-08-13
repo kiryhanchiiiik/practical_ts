e94037f03a53a145f79a14dc421c75280095d2ae
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-13 12:32:16 +0300

    Type Annotations:

---
+++ src/tutorial.ts
@@ -0,0 +1,19 @@
+let awesomeName: string = "shakeAndBake";
+
+awesomeName = "something";
+
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName = 123;
+
+let amount: number = 20;
+amount = 12 - 1;
+
+// amount = "pants";
+
+let isAwesome: boolean = true;
+
+isAwesome = false;
+
+// isAwesome = "shakeAndBake";
