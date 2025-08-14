f2431c9c4d5d5b10341e5e2ec21844ce3038e85f
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-14 12:18:11 +0300

    Type Any

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,10 +1,5 @@
-let tax: number | string = 10;
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false;
 
-tax = 100;
-tax = "$10";
-
-let requestStatus: "pending" | "success" | "error" = "pending";
-
-requestStatus = "success";
-requestStatus = "error";
-// requestStatus = "random";
+let random; // any
