537e4da009975334ba710ccd9569fbb5dfb763f6
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-15 12:15:33 +0300

    Object

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,9 +1,19 @@
-let temperatures: number[] = [20, 25, 30];
+let car: { brand: string; year: number } = {
+  brand: "toyota",
+  year: 2025,
+};
 
-// temperatures.push("twenty five");
+car.brand = "ford";
+// car.color = "blue";
 
-let colors: string[] = ["red", "green", "purple"];
+let car1: { brand: string; year: number } = {
+  brand: "audi",
+  year: 2022,
+};
 
-// colors.push(true);
+let book = { title: "book", cost: 20 };
+let pen = { title: "pen", cost: 10 };
+let notebook = { title: "notebook" };
 
-let mixArray: (number | string)[] = [1, "two", 3];
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+// items[0].title = "new book";
