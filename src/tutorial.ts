aa823d230fd15e911225e027855df8ae9f5777a0
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-16 12:09:56 +0300

    Challenge Functions 1

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,9 +1,13 @@
-function addThree(number: any) {
-  let anotherNumber: number = 3;
-  return number + anotherNumber;
+const names: string[] = ["john", "jane", "jim", "jill"];
+
+function isNameInList(name: string): boolean {
+  return names.includes(name);
 }
 
-const result = addThree(3);
-const someValue = result;
+let nameToCheck = "jane";
 
-someValue.myMethod();
+if (isNameInList(nameToCheck)) {
+  console.log(`${nameToCheck} is in the list`);
+} else {
+  console.log(`${nameToCheck} is not in the list`);
+}
