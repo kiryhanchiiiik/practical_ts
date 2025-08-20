07e9c70ccf304b2ae76941a015a8114b2dba4def
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-20 13:17:01 +0300

    Type Alias

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,16 +1,18 @@
-function processData(
-  input: string | number,
-  config: { reverse: boolean } = { reverse: false }
-): string | number {
-  if (typeof input === "number") {
-    return input * input;
-  } else {
-    return config.reverse
-      ? input.toUpperCase().split("").reverse().join("")
-      : input.toUpperCase();
-  }
-}
+type User = { id: number; name: string; isActive: boolean };
 
-console.log(processData(10));
-console.log(processData("Hello"));
-console.log(processData("Hello", { reverse: true }));
+const john: User = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+const susan: User = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: User): User {
+  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+
+  return user;
+}
