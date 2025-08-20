cb4ed1640c60e42c8236895177dfcf1e2e1514c1
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-20 13:27:44 +0300

    Challenge Type Alias

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,37 +1,32 @@
-type User = { id: number; name: string; isActive: boolean };
-
-const john: User = {
-  id: 1,
-  name: "john",
-  isActive: true,
-};
-const susan: User = {
-  id: 1,
-  name: "susan",
-  isActive: false,
+type Employee = {
+  id: number;
+  name: string;
+  department: string;
 };
 
-function createUser(user: User): User {
-  console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+type Manager = {
+  id: number;
+  name: string;
+  employees: Employee[];
+};
 
-  return user;
+type Staff = Employee | Manager;
+
+function printStaffDetails(staff: Staff): void {
+  if ("employees" in staff) {
+    console.log(
+      `${staff.name} is an manager in the ${staff.employees.length} employees`
+    );
+  } else {
+    console.log(
+      `${staff.name} is an employee in the ${staff.department} department`
+    );
+  }
 }
 
-type StringOrNumber = string | number;
+const alice: Employee = { id: 1, name: "alice", department: "Sales" };
+const steve: Employee = { id: 1, name: "steve", department: "HR" };
 
-let value: StringOrNumber;
+const bob: Manager = { id: 1, name: "bob", employees: [alice, steve] };
 
-value = "Hello";
-value = 123;
-
-type Theme = "light" | "dark";
-
-let theme: Theme;
-theme = "dark";
-theme = "light";
-
-function setTheme(t: Theme) {
-  theme = t;
-}
-
-setTheme("dark");
+printStaffDetails(bob);
