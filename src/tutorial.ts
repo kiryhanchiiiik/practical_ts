6d4543bccc43165aca6583c8a6f9f3089799447c
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-21 12:14:35 +0300

    Interface - Fundamentals

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,7 +1,16 @@
-const propName = "age";
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+}
 
-type Animal = {
-  [propName]: number;
+const deepWork: Book = {
+  isbn: 123,
+  title: "deep work",
+  author: "cal newport",
+  genre: "self-help",
 };
 
-let tiger: Animal = { [propName]: 5 };
+deepWork.title = "New Title"; // allowed
+// deepWork.isbn = 654321; // not allowed
