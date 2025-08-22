1f10048e9e5c18ce3b6af7be30753097b2d1f064
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-22 17:14:37 +0300

    Tuple

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,51 +1,16 @@
-// type Person = {
-//   name: string;
-//   age: number;
-// };
+let person: [string, number] = ["john", 25];
 
-// let john: Person = { name: "John", age: 30 };
+let date: readonly [number, number, number] = [12, 17, 2001];
+// date.push("five"); // string
+// date.push(5); // cannot
 
-// interface Person {
-//   name: string;
-//   age: number;
-// }
-
-// let john: Person = { name: "John", age: 30 };
-
-// Type alias for a primitive type
-type Score = number;
-type NumberOrString = number | string;
-// Type alias for literal types
-type Direction = "up" | "down" | "left" | "right";
-
-// Using the type aliases
-let gameScore: Score = 100;
-let move: Direction = "up";
-
-interface Person {
-  name: string;
-  greet(): void;
+function getPerson(): [string, number] {
+  return ["john", 25];
 }
 
-class Employee implements Person {
-  name: string;
+let randomPerson = getPerson();
 
-  constructor(name: string) {
-    this.name = name;
-  }
+console.log(randomPerson[0]);
+console.log(randomPerson[1]);
 
-  greet() {
-    console.log(`Hello, my name is ${this.name}`);
-  }
-}
-
-let john = new Employee("John");
-john.greet(); // Outputs: Hello, my name is John
-
-const propName = "age";
-
-type Animal = {
-  [propName]: number;
-};
-
-let tiger: Animal = { [propName]: 5 };
+let susan: [string, number?] = ["susan"];
