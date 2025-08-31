a9151795b1740a4f0d5fa297b6c8de47577f443c
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-08-31 11:55:27 +0300

    Generics - Type Constraints

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -2,4 +2,19 @@ function pair<T, U>(param1: T, param2: U): [T, U] {
   return [param1, param2];
 }
 
-let result = pair<number, string>(123, "suppp");
+// Usage
+let result = pair(123, "Hello");
+
+//  const [name,setName] = useState('')
+//  const [products,setProducts] = useState<Product[]>([])
+
+// type constraint on the generic type T, generic type can be either a number or a string.
+
+function processValue<T extends string | number>(value: T): T {
+  console.log(value);
+  return value;
+}
+
+processValue("hello");
+processValue(12);
+// processValue(true);
