4d5ffefd767ab6ee2eec0589a94ed1bee9f65fdc
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-02 13:04:40 +0300

    Fetch Data - Setup Type

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,13 +1,21 @@
-const url = "https://www.course-api.com/react-tours-projec";
+const url = "https://www.course-api.com/react-tours-project";
 
-async function fetchData(url: string) {
+type Tour = {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+};
+
+async function fetchData(url: string): Promise<Tour[]> {
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const data = await response.json();
+    const data: Tour[] = await response.json();
     console.log(data);
 
     return data;
